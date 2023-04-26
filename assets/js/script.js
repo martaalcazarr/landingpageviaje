56472d1db1966851fd35fdd924e73ceb27a16a0c
martaalcazarr <martaalcazarredondo@gmail.com>
2023-04-25 23:54:40 -0400

    navbar mejorado y arreglos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Agregar el desplazamiento suave a todos los enlaces con la clase .nav-link-smooth
+// agregar el desplazamiento suave a todos los enlaces con la clase .nav-link-smooth
 $(document).ready(function () {
   $(".nav-link-smooth").on("click", function (event) {
     if (this.hash !== "") {
@@ -17,7 +17,7 @@ $(document).ready(function () {
   });
 });
 
-// cambia la imagen cada 2 segundos
+//cambia la imagen cada 2 segundos
 $(document).ready(function () {
   $(".carousel").carousel({
     interval: 2000,
@@ -37,10 +37,37 @@ $(document).ready(function () {
 });
 
 //los iconos cambian a color negro cuando el mouse pasa por arriba (hover)
-$(document).ready(function() {
-    $('.red-social a i').hover(function() {
-      $(this).css('color', 'black');
-    }, function() {
-      $(this).css('color', 'white');
-    });
-  });
+$(document).ready(function () {
+  $(".red-social a i").hover(
+    function () {
+      $(this).css("color", "black");
+    },
+    function () {
+      $(this).css("color", "white");
+    }
+  );
+});
+
+//el fondo del navbar se vuelve negro cuando llegas a la section quienes somos, y vuelve a transparente si subes de ahí
+//en pantalla mobile se mantiene transparente
+function updateNavbarClass() {
+  var scrollPos = $(window).scrollTop();
+  var quienesSomosPos = $("#quienes-somos").offset().top;
+  var isMobile = !window.matchMedia("(min-width: 768px)").matches;
+
+  if (scrollPos >= quienesSomosPos && !isMobile) {
+    $(".navbar").addClass("bg-black");
+  } else {
+    $(".navbar").removeClass("bg-black");
+  }
+}
+// ejecutar al cargar la página
+updateNavbarClass();
+// actualizar la clase del navbar por hacer scroll o vuelve a transparente si se cambia de tamaño
+$(window).on("scroll", updateNavbarClass);
+$(window)
+  .on("resize", function () {
+    updateNavbarClass();
+    $(".navbar").removeClass("bg-black");
+  })
+  .trigger("resize");
