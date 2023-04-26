6260a9e5edbb37491847739c2257a9dfdd3ec253
martaalcazarr <martaalcazarredondo@gmail.com>
2023-04-25 20:55:24 -0400

    formulario y smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+$(document).ready(function () {
+    // Agregar el desplazamiento suave a todos los enlaces con la clase .nav-link-smooth
+    $(".nav-link-smooth").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                400,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
+});
+
 $(document).ready(function(){
     $('.carousel').carousel({
       interval: 2000 // cambia la imagen cada 2 segundos
