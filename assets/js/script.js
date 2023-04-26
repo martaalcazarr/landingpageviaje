31f229e080c63c797ae59660737950d104e1b1ee
martaalcazarr <martaalcazarredondo@gmail.com>
2023-04-25 22:34:40 -0400

    jquery y responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,46 @@
+// Agregar el desplazamiento suave a todos los enlaces con la clase .nav-link-smooth
 $(document).ready(function () {
-    // Agregar el desplazamiento suave a todos los enlaces con la clase .nav-link-smooth
-    $(".nav-link-smooth").on("click", function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $("html, body").animate(
-                {
-                    scrollTop: $(hash).offset().top,
-                },
-                400,
-                function () {
-                    window.location.hash = hash;
-                }
-            );
+  $(".nav-link-smooth").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        400,
+        function () {
+          window.location.hash = hash;
         }
-    });
+      );
+    }
+  });
 });
 
-$(document).ready(function(){
-    $('.carousel').carousel({
-      interval: 2000 // cambia la imagen cada 2 segundos
-    })
+// cambia la imagen cada 2 segundos
+$(document).ready(function () {
+  $(".carousel").carousel({
+    interval: 2000,
+  });
 });
+
+//desaparece el p cuando das click en el titulo del card
+$(document).ready(function () {
+  $(".card-title").click(function () {
+    $(".card-text").toggle();
+  });
+});
+
+//aparece tooltip en btn
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+//los iconos cambian a color negro cuando el mouse pasa por arriba (hover)
+$(document).ready(function() {
+    $('.red-social a i').hover(function() {
+      $(this).css('color', 'black');
+    }, function() {
+      $(this).css('color', 'white');
+    });
+  });
